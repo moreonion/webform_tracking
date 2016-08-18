92a94ee9a42d75bebc961d739bfdf0015e8333f2
Roman Zimmermann <roman@more-onion.com>
2016-08-18 11:41:35 +0200

    Some refactoring and tests for JS.

--- webform_tracking.js
+++ webform_tracking.js
@@ -8,6 +8,7 @@ Drupal.behaviors.webform_tracking = {
     "other"    : "o",
     "term"     : "t",
     "campaign" : "c",
+    "refsid"   : "refsid",
   },
   google_analytics: {
     "source"   : "utm_source",
@@ -24,8 +25,57 @@ Drupal.behaviors.webform_tracking = {
     }
   },
 
+  // Get white listed parameters.
+  extract_parameters: function(parameters) {
+    var data = {};
+    var map = this.extra_parameters;
+    for (var key in map) {
+      var candidates = [key, map[key], this.google_analytics[key]];
+
+      // Take first candidate key with a value.
+      for (var i = 0, l = candidates.length; i < l; i++) {
+        var value = parameters[candidates[i]];
+        if (value) {
+          data[key] = value;
+          break;
+        }
+      }
+    }
+    return data;
+  },
+
+  // Adds the URL to a history array (if needed).
+  history_add: function(history, url) {
+    var length = history.length;
+    if (length) {
+      // Only add new values (ignore page refreshes).
+      if (history[history.length-1] != url) {
+        history.push(url);
+      }
+    }
+    else {
+      length = history.push(url);
+    }
+    // Truncate the array if it gets too big.
+    if (length > 10) {
+      // We keep:
+      // [0] is the entry page
+      // [-3] might be the last page before the form == referer
+      // [-2] might be the form
+      // [-1] might be the forms /done page
+      // 10 is an arbitrary value, you just might want to avoid
+      // calling the array functions below on every request if not
+      // necessary.
+      history = [history[0]];
+      $.merge(history, history.slice(-3));
+    }
+  },
+
   run: function() {
-    var tracking_data = JSON.parse($.cookie('webform_tracking')) || {};
+    var tracking_data = JSON.parse($.cookie('webform_tracking')) || {
+      history: [],
+      tags: [],
+    };
     var parameters = this.get_url_parameters();
     var base_url = Drupal.settings.webform_tracking.base_url;
 
@@ -42,31 +92,10 @@ Drupal.behaviors.webform_tracking = {
     if (typeof parameters['tag'] !== 'undefined') {
       $.merge(tags, parameters['tag'].split(','));
     }
-    if (typeof parameters['tags'] !== 'undefined') {
-      $.merge(tags, parameters['tags'].split(','));
-    }
     tracking_data.tags = this.sort_unique(tags);
 
     // extra parameters
-    var values = this.extra_parameters;
-    for (var key in values) {
-      if (values.hasOwnProperty(key)) {
-        var result = undefined;
-        var alias = values[key];
-
-        if (typeof this.google_analytics[key] !== 'undefined') {
-          var google = this.google_analytics[key];
-        }
-
-        result = typeof parameters[google] !== 'undefined' ? parameters[google] : result;
-        result = typeof parameters[alias] !== 'undefined' ? parameters[alias] : result;
-        result = typeof parameters[key] !== 'undefined' ? parameters[key]: result;
-
-        if (typeof result !== 'undefined') {
-          tracking_data[key] = result;
-        }
-      }
-    };
+    $.merge(tracking_data, this.extract_parameters(parameters));
 
     // If the referer does not start with $base_url, it's external but we
     // only take the first external referer to avoid problems with off-site
@@ -82,26 +111,11 @@ Drupal.behaviors.webform_tracking = {
     }
 
     // history
-    var history = tracking_data.history || [];
-    var path = parameters.q || window.location.pathname.substr(1);
-    var length = history.push(base_url + '/' + path);
-    if (length > 10) {
-      // If the history is getting too long, we need at least the
-      // following:
-      // [0] is the entry page
-      // [-3] might be the last page before the form == referer
-      // [-2] might be the form
-      // [-1] might be the forms /done page
-      // 10 is an arbitrary value, you just might want to avoid
-      // calling the array functions below on every request if not
-      // necessary.
-      history = [history[0]];
-      $.merge(history, history.slice(-3));
-    }
-    tracking_data.history = history;
+    this.history_add(tracking_data.history, window.location.href);
 
     $.cookie('webform_tracking', JSON.stringify(tracking_data));
   },
+
   get_url_parameters: function() {
     var parameters = {};
     var variables = window.location.search.substring(1).split('&');
