5bd1c3aa57a2c66c2a014343942e8491a8ebdc53
Roman Zimmermann <roman@more-onion.com>
2021-05-11 09:15:05 +0200

    Implement webform results in a way that works with any patch version.
    - Unpatched webform.
    - With old versions of #2117285 (up to and including comment 49).
    - With newer version (based on #3086038).

---
+++ tests/WebformTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Drupal\webform_tracking;
+
+use Upal\DrupalUnitTestCase;
+
+/**
+ * Test the webform hook implementations.
+ */
+class WebformTest extends DrupalUnitTestCase {
+
+  /**
+   * Test getting the submission data with the patch from #2117285.
+   *
+   * @link https://www.drupal.org/node/2117285 #2117285 @endlink
+   */
+  public function testSubmissionDataWith2117285() {
+    $submission = (object) ['tracking' => []];
+    $submission->tracking['tags'] = ['foo', 'bar', 'baz'];
+    $data = webform_tracking_webform_results_download_submission_information_data($submission, [], 0, 1);
+    $this->assertEqual([
+      'webform_tracking_tags' => 'foo, bar, baz',
+    ], $data);
+  }
+
+  /**
+   * Test getting the submission data with the patch from #3086038.
+   *
+   * @link https://www.drupal.org/node/3086038 #3086038 @endlink
+   */
+  public function testSubmissionDataWith3086038() {
+    $submission = (object) ['tracking' => []];
+    $submission->tracking['tags'] = ['foo', 'bar', 'baz'];
+    $data = webform_tracking_webform_results_download_submission_information_data_row($submission, [], 0, 1);
+    $this->assertEqual([
+      'webform_tracking_tags' => 'foo, bar, baz',
+    ], $data);
+  }
+
+  /**
+   * Test getting the submission data without the patch from #3086038.
+   */
+  public function testSubmissionDataWithoutPatch() {
+    $submission = (object) ['tracking' => []];
+    $submission->tracking['tags'] = ['foo', 'bar', 'baz'];
+    $data = webform_tracking_webform_results_download_submission_information_data('webform_tracking_tags', $submission, [], 0, 1);
+    $this->assertEqual('foo, bar, baz', $data);
+  }
+
+}
